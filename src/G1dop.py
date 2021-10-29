62255ddea3cb3421b5641bd1399f171da3f863f5
Ngor <nikitagorin034@gmail.com>
2021-10-29 14:18:10 +0400

    валидация, poetry, def

--- src/G1dop.py
+++ src/G1dop.py
@@ -9,3 +9,12 @@ def get_square(empty):
         return(result)
 square1 = get_square(empty)
 print(square1)
+
+try:
+    empty = int(string_empty)
+except ValueError:
+    raise ValueError(f'Не могу превратить значение {string_empty} в число')
+if empty <= 0:
+    raise ValueError('Сторона квадрата должна быть больше нуля')
+
+square1 = get_square(empty)
